674995342e397c65430a4e5f33fd33285b62d65b
motorbreath13eh <120326142+motorbreath13eh@users.noreply.github.com>
2023-01-22 12:21:22 +0200

    Update Random_card_game.c

--- Random_card_game.c
+++ Random_card_game.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> // για τη rand()
-#include <conio.h> // για τη getch()
+#include <time.h> // for rand() and srand()
+#include <conio.h> // for getch()
 
-// global variables and arrays
+// Declaration of global variables and arrays
 int i,j;
 char x[] = {'A','2','3','4','5','6','7','8','9','1','0','J','Q','K','\0'};
 char s[]= {'\3','\4','\5','\6','\0'};
 
-// declaration of methods
+// Calling our methods
 void trapouloxarto();
 void trapouloxarto2();
 
 int main()
 {
-	char n; // η char μεταβλητή για την επιλογή χαρακτήρα '0' ή χαρακτήρα '1'
+	char n; // char variable for user's choice '0' or '1'
 	do
 	{
 		trapouloxarto();
-		n = getch(); // εισάγουμε χαρακτήρα '0' ή χαρακτήρα '1' χωρίς ENTER!!!
-	}while (n=='0'); // όταν n=='0', τερματίζει το πρόγραμμα!!
+		n = getch(); // The user can enter character '0' or character '1' without ENTER!!!
+	}while (n=='0'); // When n=='0', the program terminates!!
 	
 	system("pause");
 	return 0;
@@ -30,19 +30,19 @@ void trapouloxarto()
 	srand(time(NULL));
 	system("cls");
 	
-	i = rand()%14; // for 14 choices of i, για τα στοιχεία του πίνακα x[]
-	j = rand()%4; // for 4 choices of j, για τα 4 στοιχεία του πίνακα s[]
+	i = rand()%14; // for 14 choices of i, of the 14 elements of array x[]
+	j = rand()%4; // for 4 choices of j, of the 4 elements of array s[]
 	
-	// για την περίπτωση που το τραπουλόχαρτο έχει αριθμό 10 καλούμε τη συνάρτηση trapouloxarto2()
+	// In case the card has the number 10, we call the method "trapouloxarto2()"
 	if (i==10 || i==11)
 	{
 		trapouloxarto2();
 	}
 	else
 	{
-		if (j==0 || j==1) // για κούπες και καρό
+		if (j==0 || j==1) // for hearts and diamons
 		{
-			system("color f4");  // άσπρο background, κόκκινο χρώμα γράμματα
+			system("color f4");  // white background, red text
 			printf("\t\t\t ___________________ \n");
 			printf("\t\t\t|                   |\n");
 			printf("\t\t\t|   %c               |\n",x[i]);
@@ -57,11 +57,11 @@ void trapouloxarto()
 			printf("\t\t\t|               %c   |\n",s[j]);
 			printf("\t\t\t|               %c   |\n",x[i]);
 			printf("\t\t\t|___________________|\n\n\n");
-			printf("Next card: Nai(0) , Exit(1))");
+			printf("Next card: Yes(0) , Exit(1))");
 		}
-		else if (j==2 || j==3) // για μπαστούνια και σπαθιά
+		else if (j==2 || j==3) // for clubs and spades
 		{
-			system("color f0"); // άσπρο background, μαύρο χρώμα γράμματα
+			system("color f0"); // white background, black text
 			printf("\t\t\t ___________________ \n");
 			printf("\t\t\t|                   |\n");
 			printf("\t\t\t|   %c               |\n",x[i]);
@@ -76,20 +76,20 @@ void trapouloxarto()
 			printf("\t\t\t|               %c   |\n",s[j]);
 			printf("\t\t\t|               %c   |\n",x[i]);
 			printf("\t\t\t|___________________|\n\n\n");
-			printf("Next card: Nai(0) , Exit(1))");
+			printf("Next card: Yes(0) , Exit(1))");
 		}
 	}
 }
 
-void trapouloxarto2() // για την περίπτωση που το τραπουλόχαρτο έχει αριθμό 10
+void trapouloxarto2() // In case the random card picks the number 10
 {
 	srand(time(NULL));
 	system("cls");
 	
 	j = rand()%4;
-	if (j==0 || j==1) // για κούπες και καρό
+	if (j==0 || j==1) // for hearts and diamonds
 		{
-			system("color f4");  // άσπρο background, κόκκινο χρώμα γράμματα
+			system("color f4");  // white background, red text
 			printf("\t\t\t ___________________ \n");
 			printf("\t\t\t|                   |\n");
 			printf("\t\t\t|  %c%c               |\n",x[9],x[10]);
@@ -104,11 +104,11 @@ void trapouloxarto2() // για την περίπτωση που το τραπουλόχαρτο έχει αριθμό 10
 			printf("\t\t\t|               %c   |\n",s[j]);
 			printf("\t\t\t|               %c%c  |\n",x[9],x[10]);
 			printf("\t\t\t|___________________|\n\n\n");
-			printf("Next card: Nai(0) , Exit(1))");
+			printf("Next card: Yes(0) , Exit(1))");
 		}
-		else if (j==2 || j==3)  // για μπαστούνια και σπαθιά
+		else if (j==2 || j==3)  // for clubs and spades
 		{
-			system("color f0"); // άσπρο background, μαύρο χρώμα γράμματα
+			system("color f0"); // white background, black text
 			printf("\t\t\t ___________________ \n");
 			printf("\t\t\t|                   |\n");
 			printf("\t\t\t|  %c%c               |\n",x[9],x[10]);
@@ -123,6 +123,6 @@ void trapouloxarto2() // για την περίπτωση που το τραπουλόχαρτο έχει αριθμό 10
 			printf("\t\t\t|               %c   |\n",s[j]);
 			printf("\t\t\t|               %c%c  |\n",x[9],x[10]);
 			printf("\t\t\t|___________________|\n\n\n");
-			printf("Next card: Nai(0) , Exit(1))");
+			printf("Next card: Yes(0) , Exit(1))");
 		}
 }
